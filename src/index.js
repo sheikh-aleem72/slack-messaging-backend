a05b3f05df9f94a836869daf0b3700ee4ebe6e74
aleem <sheikhaleem363@gmail.com>
2024-12-05 08:29:22 +0530

    feat: Added CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+// import bullServerAdapter from "./config/billBoardConfig.js";
+import cors from "cors";
 import express from "express";
 import { createServer } from "http";
 import { StatusCodes } from "http-status-codes";
 import { Server } from "socket.io";
 
-// import bullServerAdapter from "./config/billBoardConfig.js";
 import connectDB from "./config/dbConfig.js";
 import { PORT } from "./config/serverConfig.js";
 import channelSocketHandlers from "./controllers/channelSocketController.js";
@@ -14,6 +15,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
