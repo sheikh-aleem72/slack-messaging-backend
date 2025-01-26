0ebccb414799f08677e1acf216ec27e0ef783fea
aleem <sheikhaleem363@gmail.com>
2025-01-26 09:37:59 +0530

    feat: Add presigned url logic

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -6,6 +6,8 @@ import {
   internalServerErrror,
   successResponse,
 } from "../utils/common/responseObject.js";
+import { s3 } from "../config/awsConfig.js";
+import { AWS_BUCKET_NAME } from "../config/serverConfig.js";
 
 export const getMessage = async (req, res) => {
   try {
@@ -23,6 +25,28 @@ export const getMessage = async (req, res) => {
       .json(successResponse(messages, "Messages fetched successfully"));
   } catch (error) {
     console.log("Error from getMessageController", error);
+    if (error.status) {
+      return res.status(error.status).json(errorReponse(error));
+    }
+
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalServerErrror(error));
+  }
+};
+
+export const getPresignedUrlFromAWS = async (req, res) => {
+  try {
+    const url = await s3.getSignedUrlPromise("putObject", {
+      Bucket: AWS_BUCKET_NAME,
+      Key: `${Date.now()}`,
+      Expires: 60, // 1 minute
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json(successResponse(url, "Presigned URL generated successfully"));
+  } catch (error) {
+    console.log("Error in getPresignedUrlFromAWS", error);
     if (error.status) {
       return res.status(error.status).json(errorReponse(error));
     }
--- src/routes/v1/messages.js
+++ src/routes/v1/messages.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { getMessage } from "../../controllers/messageController.js";
+import {
+  getMessage,
+  getPresignedUrlFromAWS,
+} from "../../controllers/messageController.js";
 import { isAuthenticate } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/:channelId", isAuthenticate, getMessage);
+
+// router.get('/pre-signed-url', isAuthenticate, getPresignedUrlFromAWS); // For presigned url
 
 export default router;
